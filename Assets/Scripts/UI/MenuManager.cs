fe0d429ba4be518eecaa994368acb20e7e6bad53
Angelina Casanova <anghjulinacasa@gmail.com>
2026-02-01 15:07:21 +0100

    Add audio and button UI enhancements to Start Menu
    Introduced audio assets and a mixer for UI sounds, replaced UIAnimationManager with UIButtonManager to handle button hover and click effects with sound, and updated StartMenu scene to integrate these changes. Also adjusted UI layout, and improved credits and title display.

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -2,30 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Configuration.Assemblies;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    private Canvas menuCanvas;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void onPlayClicked()
     {
+        StartCoroutine(WaitDelay(3f));
         UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+
+    IEnumerator WaitDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
     }
 
     public void onQuitClicked()
     {
+        StartCoroutine(WaitDelay(3f));
         Application.Quit();
     }
 
---
+++ Assets/Scripts/UI/UIButtonManager.cs
@@ -0,0 +1,61 @@
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine;
+
+[RequireComponent(typeof(Button))]
+public class UIButtonManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    private TextMeshProUGUI text;
+    [Header("Audio")]
+    [SerializeField] private AudioClip hoverSound;
+    [SerializeField] private AudioClip clickSound;
+    [SerializeField] private AudioMixerGroup menuMixerGroup;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.outputAudioMixerGroup = menuMixerGroup;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerEnter.name == "TextPlay")
+        {
+            text.text = "> JOUER";
+        }
+        else if (eventData.pointerEnter.name == "TextQuit")
+        {
+            text.text = "> QUITTER";
+        }
+        if (hoverSound != null && GetComponent<Button>().interactable)
+            audioSource.PlayOneShot(hoverSound);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerEnter.name == "TextPlay")
+        {
+            text.text = "JOUER";
+        }
+        else if (eventData.pointerEnter.name == "TextQuit")
+        {
+            text.text = "QUITTER";
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (clickSound != null && GetComponent<Button>().interactable)
+            audioSource.PlayOneShot(clickSound);
+    }
+
+}
